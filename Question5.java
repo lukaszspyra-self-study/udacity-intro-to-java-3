c7e258b848d075450f8d5b625a61db9bf2ac4150
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-02-10 17:54:40 +0100

    Answer to Task5 added

---
+++ Question5.java
@@ -0,0 +1,11 @@
+public class Question5 {
+
+    public static void main(String[] args){
+
+        public double calculateTip(double bill){
+            
+            return 0.15 * bill;
+
+        }
+    }
+}
