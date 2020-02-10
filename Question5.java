09e487c13167ebd7654ff89260d684be4b8a7de2
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-02-10 18:24:28 +0100

    Removed method main from 5 and 7 method exercises

--- Question5.java
+++ Question5.java
@@ -1,11 +1,6 @@
 public class Question5 {
 
-    public static void main(String[] args){
-
-        public double calculateTip(double bill){
-            
-            return 0.15 * bill;
-
-        }
+    public double calculateTip(double bill){        
+        return 0.15 * bill;
     }
 }
--- Question7.java
+++ Question7.java
@@ -1,23 +1,20 @@
 public class Question7 {
 
-    public static void main(String[] args){
+    /**
+    *Converts temperature from Fahrenheit to Celsius
+    *@param tempInFahrenheit
+    *@return calculated temperature in degrees Celsius
+    */
+    public double fahrenheitToCelsius(double tempInFahrenheit){
+        return (tempInFahrenheit - 32) * 5 / 9;
+    }
 
-        /**
-        *Converts temperature from Fahrenheit to Celsius
-        *@param tempInFahrenheit
-        *@return calculated temperature in degrees Celsius
-        */
-        public double fahrenheitToCelsius(double tempInFahrenheit){
-            return (tempInFahrenheit - 32) * 5 / 9;
-        }
-
-        /**
-        *Prints temperature in both degrees Fahrenheit and Celsius
-        *@param tempInFahrenheit
-        */
-        public void printTemperature(double tempInFahrenheit){
-            System.out.println("F: " + tempInFahrenheit);
-            System.out.println("C: " + fahrenheitToCelsius(tempInFahrenheit);)
-        }
+    /**
+    *Prints temperature in both degrees Fahrenheit and Celsius
+    *@param tempInFahrenheit
+    */
+    public void printTemperature(double tempInFahrenheit){
+        System.out.println("F: " + tempInFahrenheit);
+        System.out.println("C: " + fahrenheitToCelsius(tempInFahrenheit);)
     }
 }
